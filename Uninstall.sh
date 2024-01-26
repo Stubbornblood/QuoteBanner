58d79b8e84d0fa8dd040224f65b784017f2b8ad7
Hare Krishna <shivanshu singh>
2024-01-26 23:40:03 +0530

    Uinstall file updated

--- Uninstall.sh
+++ Uninstall.sh
@@ -1,23 +1,27 @@
 folder_remove() {
-cd $HOME
-if [[ -e QuoteBanner]]
-then
-rm -rvf QuoteBanner
-fi
+    cd $HOME
+    if [[ -e "QuoteBanner" ]]
+    then
+        rm -rvf QuoteBanner
+    fi
 }
+
 install() {
-echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then>         command_not_found_handle() {
-/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+    echo 'if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+        command_not_found_handle() {
+            /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+        }
+    fi
+    PS1='\$ '' > /data/data/com.termux/files/usr/etc/zshrc
+
+    echo 'if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+        command_not_found_handle() {
+            /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+        }
+    fi
+    PS1='\$ '' > /data/data/com.termux/files/usr/etc/bash.bashrc
 }
-fi
-PS1='\$ '" > /data/data/com.termux/files/usr/etc/zshrc
-echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
-command_not_found_handle() {
-/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
-}
-fi
-PS1='\$ '" > /data/data/com.termux/files/usr/etc/bash.bashrc
-}
+
 folder_remove
 install
 folder_remove
