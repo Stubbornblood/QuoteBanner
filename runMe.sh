032a09f94e3af3c110fc2fe519c5e305c2100c66
Hare Krishna <shivanshu singh>
2024-01-26 23:49:17 +0530

    Uinstall file updated

--- runMe.sh
+++ runMe.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
-
 get_quote() {
     quote=$(curl -s 'https://api.quotable.io/random' | jq -r '.content')
     echo "$quote"
 }
 
-
 quote=$(get_quote)
-
 
 cowsay -f dragon "$quote"
 
+cyan='\033[0;36m'
+lightgreen='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
 
-cyan='[0;36m'
-lightgreen='[1;32m'
-red='[1;31m'
-yellow='[1;33m'
-echo -e $lightgreen "[1m                Haha Boi gonna rock... "
+echo -e "${lightgreen}[1m                Haha Boi gonna rock... "
 echo " "
-echo -e $yellow "[1m                         -Shivanshu Singh "
+echo -e "${yellow}[1m                         -Shivanshu Singh "
 echo " "
 echo " "
 echo " "
-echo -e "[1m[33m
-What is Your [31mBanner [33mName[32m :
 
-"
+echo -e "${yellow}[1mWhat is Your ${red}Banner ${yellow}Name: "
 read varbanner
 echo
-echo -e "[1m[33m
-What is Your Cowsay Name[32m :
 
- "
+echo -e "${yellow}[1mWhat is Your Cowsay Name: "
 read varcowsay
-read quote
-echo 'get_quote() { quote=$(curl -s "https://api.quotable.io/random" | jq -r ".content"); cowsay -f eyes "$quote"; }' | lolcat" > DragonSay.txt
-echo 'toilet -f big ' $varbanner' -F gay | lolcat' > TextOp.txt
-echo
-echo "clear" > Unistall.txt
-cat "Unistall.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+echo 'get_quote() { quote=$(curl -s "https://api.quotable.io/random" | jq -r ".content"); cowsay -f eyes "$quote"; }' > DragonSay.txt
+echo 'toilet -f big "${varbanner}" -F gay | lolcat' > TextOp.txt
+echo 'clear' > Uninstall.txt
+
+cat "Uninstall.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 cat "DragonSay.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-ls
 cat "TextOp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
