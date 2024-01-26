b3cc3ee12848a0f19b996d47962cd0d7451a8044
Hare Krishna <shivanshu singh>
2024-01-27 01:42:44 +0530

    Done Username

--- newRunme.sh
+++ newRunme.sh
@@ -6,7 +6,8 @@ cp "$new_bashrc" /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "bash.bashrc replaced successfully!"
 
 read -p "Enter your name: " user_name
-echo 'toilet -f big "${user_name}" -F gay | lolcat' > TextOp.txt
-cat "TextOp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+command="toilet -f big '$user_name' -F gay | lolcat"
+echo "$command" > TextOp.txt
+echo "$command" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 # echo 'user_name=' $user_name >> /data/data/com.termux/files/usr/etc/name.sh
 # echo 'export "user_name"'>> /data/data/com.termux/files/usr/etc/name.sh
