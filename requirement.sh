9007690198fd4fdbe355f21a356fdbdaf7663b59
Hare Krishna <shivanshu singh>
2024-01-26 20:14:13 +0530

    first commit

---
+++ requirement.sh
@@ -0,0 +1,15 @@
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "[1m Now Run bash runMe.sh...!!!"
+echo 
+echo -e 
