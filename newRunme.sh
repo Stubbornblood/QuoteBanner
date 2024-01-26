24018aa2058a0d090bf247140973af9ea74e7972
Hare Krishna <shivanshu singh>
2024-01-27 00:52:17 +0530

    Done with New Update

--- newRunme.sh
+++ newRunme.sh
@@ -4,3 +4,7 @@ new_bashrc="./bash.bashrc"
 cp /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc.bak
 cp "$new_bashrc" /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "bash.bashrc replaced successfully!"
+
+read -p "Enter your name: " user_name
+echo '$user_name=' $user_name >> /data/data/com.termux/files/usr/etc/name.sh
+echo 'export $user_name' >> /data/data/com.termux/files/usr/etc/name.sh
