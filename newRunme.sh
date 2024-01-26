2b7ae46ef14f2c4cbb36a9d93eb14ea73ed4a006
Hare Krishna <shivanshu singh>
2024-01-27 01:55:28 +0530

    Done

--- newRunme.sh
+++ newRunme.sh
@@ -3,9 +3,10 @@
 new_bashrc="./bash.bashrc"
 cp /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc.bak
 cp "$new_bashrc" /data/data/com.termux/files/usr/etc/bash.bashrc
-echo "Installation done now restart your termux"
 
 read -p "Enter your banner name: " user_name
 command="toilet -f big '$user_name' -F gay | lolcat"
 echo "$command" > TextOp.txt
 echo "$command" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+echo "Installation done now restart your termux"
